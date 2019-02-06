285364c21b35da63c4968059c4e26f20969dbad1
caleb frumkin <calebfrumkin@gmail.com>
2019-02-05 22:51:51 -0500

    Initial Version

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    
+
+    void OnCollisionEnter(Collision collisionInfo){
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Restart : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        if (Input.GetKey("r"))
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
