6424f9267b2c7957e6379eab5d8d8ec0e465a583
caleb frumkin <calebfrumkin@gmail.com>
2019-02-06 21:39:33 -0500

    version2.0

---
+++ Assets/Scripts/startButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class startButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Begin()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    
+}
